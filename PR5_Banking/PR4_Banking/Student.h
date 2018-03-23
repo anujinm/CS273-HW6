5224e0445843f1d252159e9ada4a3f1d21136f6b
Anujin Munkhbat <anujin.munhbat@gmail.com>
2018-03-22 22:32:53 -0700

    Update Student.h

--- PR5_Banking/PR4_Banking/Student.h
+++ PR5_Banking/PR4_Banking/Student.h
@@ -7,11 +7,13 @@
 class Student : public Customer {
 public:
 	//constructor
-	Student(std::string name, std::string address, int age, std::string tel_number, int customer_id : Customer name, address, age, tel_number, customer_id) {
+	Student(std::string name, std::string address, int age, std::string tel_number, int customer_id) : Customer (name, address, age, tel_number, customer_id) {
 		checking_int_rate = 0.45;
 		saving_int_rate = 0.7;
 	}
 	~Student() {}
 };
 #endif // !STUDENT_H
+
+
 
